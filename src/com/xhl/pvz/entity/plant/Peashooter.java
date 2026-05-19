e13d6560cfbb569bf9763c7fc63fc1f470951dcd
DM001-mm <Xuhaolin2918782608@outlook.com>
2026-05-19 16:50:42 +0800

    豌豆射手的shoot函数没想象中的那么复杂，有EntityManager，一切都水到渠成

--- src/com/xhl/pvz/entity/plant/Peashooter.java
+++ src/com/xhl/pvz/entity/plant/Peashooter.java
@@ -1,22 +1,44 @@
 package com.xhl.pvz.entity.plant;
 
 import com.xhl.pvz.core.LevelContext;
+import com.xhl.pvz.entity.bullet.PeaBullet;
+import com.xhl.pvz.manager.AudioManager;
 import com.xhl.pvz.manager.ImageManager;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 
 public class Peashooter extends Plant {
     private BufferedImage image;
-    
+    private final int shootInterval =60;
+    private int shootTimer = 0;
+
     public Peashooter(int row,int col,double x,double y){
         super(row,col,x,y,80,80,300,100,150);
         image = ImageManager.getImage("plant.peashooter.idle.0");
     }
     @Override
     public void update(LevelContext context){
+        shootTimer++;
 
+        boolean hasZombie =context.getEntityManager().hasZombieInRow(row, x);
+        if(hasZombie&&shootTimer>=shootInterval){ // 这里的射击机制有点奇怪
+            shootTimer =0; 
+            shoot(context); 
+        }
     }
     
+    private void shoot(LevelContext context){
+        double bulletX =x+55;
+        double bulletY =y+20;
+
+        PeaBullet bullet =new PeaBullet(shootInterval, bulletX, bulletY); //666 后面直接添加到 entitymanager里面
+        context.getEntityManager().addBullet(bullet);
+
+        AudioManager.playEffect("peashoot");
+        
+        System.out.println("豌豆射手发射豌豆");//sb
+        
+    }
     @Override
     public void render(Graphics2D g){
         g.drawImage(image,(int)x,(int)y,width,height,null);
