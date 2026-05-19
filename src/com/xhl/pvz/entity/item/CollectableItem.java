2327cdcfdbe2d9fd9f276c6e81248d7e3a44bea3
DM001-mm <Xuhaolin2918782608@outlook.com>
2026-05-19 14:12:44 +0800

    填写了CollectableItem类

--- src/com/xhl/pvz/entity/item/CollectableItem.java
+++ src/com/xhl/pvz/entity/item/CollectableItem.java
@@ -3,4 +3,21 @@ package com.xhl.pvz.entity.item;
 import com.xhl.pvz.entity.Entity;
 
 public abstract class CollectableItem extends Entity {
+    protected int value;
+    
+    public CollectableItem(double x,double y,int width,int height,int value){
+        super(x,y,width,height);
+        this.value=value;
+    }
+
+    public boolean contains(int mouseX,int mouseY){
+        return getBounds().contains(mouseX,mouseY);
+    }
+
+    public void collect(){
+        alive= false;
+    }
+    public int getValue(){
+        return value;
+    }
 }
