8af2e7a6e14a482c7e3198d299365f2e3b6fb10e
DM001-mm <Xuhaolin2918782608@outlook.com>
2026-06-10 10:06:19 +0800

    解决了大厅没有背景音乐的问题

--- src/com/xhl/pvz/scene/MainMenuScene.java
+++ src/com/xhl/pvz/scene/MainMenuScene.java
@@ -2,6 +2,7 @@ package com.xhl.pvz.scene;
 
 import com.xhl.pvz.core.GameConfig;
 import com.xhl.pvz.core.SceneManager;
+import com.xhl.pvz.manager.AudioManager;
 import com.xhl.pvz.manager.ImageManager;
 import com.xhl.pvz.resource.ImageKeys;
 import com.xhl.pvz.ui.UIButton;
@@ -19,9 +20,11 @@ public class MainMenuScene extends BaseScene {
     @Override
     public void onEnter(){ // 这个是 给 Manager 中的 change 操作使用的 ，就是不是 主动调用的，至于在哪里调用，当然是通过事件响应机制调用了
         System.out.println("进入主菜单场景");
-        background = ImageManager.getImage(ImageKeys.BACKGROUND_MAIN_MENU);//
+        if (ImageManager.hasImage(ImageKeys.BACKGROUND_MAIN_MENU)) {
+            background = ImageManager.getImage(ImageKeys.BACKGROUND_MAIN_MENU);//
+        }
         startButton = new UIButton(425,80,390,120,ImageManager.getImage(ImageKeys.UI_START_BUTTON));
-        // 如果 需要 ，这里可以加上 声音
+        AudioManager.playBGM("main_menu");
     } // 进入只是 为了加载一些功能，画面渲染 分给了 render函数
     @Override
     public void onExit(){
@@ -40,6 +43,7 @@ public class MainMenuScene extends BaseScene {
     @Override
     public void onMousePressed(int x,int y){
         if(startButton.contains(x,y)){
+            AudioManager.playEffect("click");
             sceneManager.changeScene(new LevelScene(sceneManager)); // 改进入 选择 游戏 关卡 界面了
         }
     }// 你想过了 没有 如果让你来写 你会怎么样，类是类，对象是对象，实际做事的 是对象，你知道了，但是 工程中你又 怎么 才能 用到呢？ 这里就用到了，如果真的按照你理解的面向对象的思想，
